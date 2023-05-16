969f752cfe9bb460d312f3e2d44a7f2dd30736cb
UriB1 <106737885+UriB1@users.noreply.github.com>
2023-05-16 17:51:09 +0300

    Updated ServerManager and Constants

--- bookscrabble/src/main/java/project/client/model/ConnectionModel.java
+++ bookscrabble/src/main/java/project/client/model/ConnectionModel.java
@@ -1,7 +1,5 @@
 package project.client.model;
 
-import project.server.serverHandler.ServerManager;
-
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
@@ -10,61 +8,73 @@ import java.util.Observable;
 import java.util.Scanner;
 
 public class ConnectionModel extends Observable {
-    String hostAddress;
-    final int hostPort = 5000;
+    ServerSocket hostInputSocket;
+    String hostAddress, managerAddress;
     String hostResponse, serverResponse;
+    private final int HOST_PORT = 5001, MANAGER_PORT = 5002;
 
-    public ConnectionModel(String hostAddress) {this.hostAddress = hostAddress;}
+    public ConnectionModel(String hostAddress, String serverAddress) {
+        this.hostAddress = hostAddress;
+        this.managerAddress = serverAddress;
+    }
 
     public String getHostResponse() {return hostResponse;}
 
     public String getServerResponse() {return serverResponse;}
 
     public void modelCommunications(String message) {
-        if (!hostAddress.equals("localhost")) {
-            try (Socket socketToHost = new Socket(hostAddress, hostPort);
-                 Scanner inFromHost = new Scanner(socketToHost.getInputStream());
-                 PrintWriter outToHost = new PrintWriter(socketToHost.getOutputStream())) {
-                outToHost.println(message);
-                outToHost.flush();
-                if (inFromHost.hasNext()) {
-                    hostResponse = inFromHost.next();
-                    setChanged();
-                    notifyObservers();
-                }
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        }
+        if (!hostAddress.equals("localhost"))
+            clientCommunications(message);
+        else
+            hostCommunications();
+    }
 
-        else {
-            try {
-                Socket clientSocket;
-                try (ServerSocket hostInputSocket = new ServerSocket(hostPort)) {
-                    clientSocket = hostInputSocket.accept();
-                }
-
-                Scanner inFromClient = new Scanner(clientSocket.getInputStream());
-                PrintWriter outToClient = new PrintWriter(clientSocket.getOutputStream());
-                String clientMessage = inFromClient.next();
-
-                Socket hostOutputSocket = new Socket();
-                ServerManager manager = new ServerManager(hostOutputSocket);
-                if (clientMessage.startsWith("1") || clientMessage.startsWith("2"))
-                    serverResponse = manager.messageToGameServer(clientMessage);
-                else
-                    serverResponse = manager.messageToBookScrabbleServer(clientMessage);
-
-                outToClient.println(serverResponse);
-                outToClient.flush();
+    private void clientCommunications(String message) {
+        try (Socket socketToHost = new Socket(hostAddress, HOST_PORT);
+             Scanner inFromHost = new Scanner(socketToHost.getInputStream());
+             PrintWriter outToHost = new PrintWriter(socketToHost.getOutputStream())) {
+            outToHost.println(message);
+            outToHost.flush();
+            if (inFromHost.hasNext()) {
+                hostResponse = inFromHost.next();
                 setChanged();
                 notifyObservers();
-                inFromClient.close();
-                outToClient.close();
-                clientSocket.close();
-            } catch (IOException e) {
-                throw new RuntimeException(e);
             }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void hostCommunications() {
+        try {
+            hostInputSocket = new ServerSocket(HOST_PORT);
+            hostInputSocket.setSoTimeout(2000);
+            Socket clientSocket = hostInputSocket.accept();
+
+            Scanner inFromClient = new Scanner(clientSocket.getInputStream());
+            PrintWriter outToClient = new PrintWriter(clientSocket.getOutputStream());
+            String clientMessage = inFromClient.next();
+
+            Socket managerSocket = new Socket(managerAddress, MANAGER_PORT);
+            Scanner fromManager = new Scanner(managerSocket.getInputStream());
+            PrintWriter toManager = new PrintWriter(managerSocket.getOutputStream());
+            toManager.println(clientMessage);
+            toManager.flush();
+            if (fromManager.hasNext())
+                serverResponse = fromManager.next();
+            fromManager.close();
+            toManager.close();
+            managerSocket.close(); // need to create a condition to close
+
+            outToClient.println(serverResponse);
+            outToClient.flush();
+            setChanged();
+            notifyObservers();
+            inFromClient.close();
+            outToClient.close();
+            clientSocket.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 }
--- bookscrabble/src/main/java/project/server/serverHandler/ServerManager.java
+++ bookscrabble/src/main/java/project/server/serverHandler/ServerManager.java
@@ -1,39 +1,119 @@
 package project.server.serverHandler;
 
+import project.server.Constants;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketTimeoutException;
 import java.util.HashMap;
 import java.util.Scanner;
 
 public class ServerManager {
-    HashMap<ServerManager,Socket> games;
-    MyServer BookScrabbleServer, GameServer; // Servers
-    String bookScrabbleServerName, gameServerName; // IP
-    final int BookScrabblePort = 5001, GamePort = 5002; // Ports
-    Socket socket; // Host's socket
+    private HashMap<String, MyServer> gameServers;
+    private ServerSocket managerSocket;
+    private MyServer BookScrabbleServer; // This server is used by many hosts of unrelated games
+    private final int MANAGER_PORT = 5002, BookScrabblePort = 5003, STARTING_GAME_PORT = 5004; // Ports
+    private int gameCounter = 0; // Counter to assign unique game IDs
+    Boolean closeServerManager = false;
 
-    public ServerManager(Socket socket) { // Ctor
-        this.socket = socket;
-        BookScrabbleServer = new MyServer(BookScrabblePort , new BookScrabbleHandler()); // This server is used by many hosts of unrelated games
-        BookScrabbleServer.start(); // Always runs in the background
-        GameServer = new MyServer(GamePort, new GameHandler());
-        GameServer.start();
+    public ServerManager() { // Ctor
+        gameServers = new HashMap<>();
+        try {
+            managerSocket = new ServerSocket(MANAGER_PORT);
+            System.out.println("ServerManager: Listening on port " + MANAGER_PORT);
+            BookScrabbleServer = new MyServer(BookScrabblePort , new BookScrabbleHandler());
+            BookScrabbleServer.start(); // Always runs in the background
+            Thread connectionThread = new Thread(this::messageManagement);
+            connectionThread.start();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
-     public String messageToBookScrabbleServer (String message) { // A method to send a query/challenge to the book scrabble server
-         return messageMethod(message, bookScrabbleServerName, BookScrabblePort); // Answer from server
+     public void messageManagement () { // A method to classify a message and send it to a specific client handler
+        try {
+            managerSocket.setSoTimeout(2000);
+            while(!closeServerManager)
+            {
+                try {
+                    Socket socket = managerSocket.accept();
+                    try (socket; Scanner fromHost = new Scanner(socket.getInputStream());
+                         PrintWriter toHost = new PrintWriter(socket.getOutputStream())) {
+
+                        String fullMessage = fromHost.next(); // hostID&message
+                        String[] split = fullMessage.split("&");
+                        String hostID = split[0];
+                        String message = split[1];
+
+                        if (!gameServers.containsKey(hostID)) // If hostID isn't in gameServers
+                            gameServers.put(hostID, null);
+
+                        serverClassification(hostID, message, toHost);
+                        toHost.flush();
+                    }
+                } catch (SocketTimeoutException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            managerSocket.close();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
      }
 
-     public String messageToGameServer (String message) { // A method to send a String to the game server
-         return messageMethod(message,  gameServerName, GamePort); // Returns String according to the servers answer
-     }
+    private void serverClassification(String hostID, String message, PrintWriter toHost) {
+        char firstLetter = message.charAt(0);
+        String subMessage = message.substring(2);
 
-    private String messageMethod (String message, String serverName, int port) { // A method that communicates with the MyServer instances
+        switch(firstLetter) {
+            case '1':
+                char letter = subMessage.charAt(0);
+                switch(letter) {
+                    case 'N':
+                        if (gameServers.get(hostID) == null) {
+                            gameServers.replace(hostID, new MyServer(STARTING_GAME_PORT + gameCounter, new GameHandler()));
+                            gameCounter++;
+                        }
+                        toHost.println(Constants.OK);
+                        break;
+                    case 'R':
+                        int port = gameServers.get(hostID).getPort();
+                        gameServers.replace(hostID, new MyServer(port, new GameHandler()));
+                        toHost.println(Constants.OK);
+                        break;
+                    case 'D':
+                        gameServers.get(hostID).close();
+                        gameServers.remove(hostID);
+                        break;
+                    case 'E':
+                        //...Maybe to do
+                        break;
+                    default:
+                        break;
+                }
+                break;
+            case '2':
+                String response = messageMethod(subMessage, gameServers.get(hostID).getPort());
+                String[] arrResponse = response.split(",");
+                if(arrResponse[0].equals("600")) {
+                    gameServers.get(hostID).close();
+                    gameServers.remove(hostID);
+                }
+                toHost.println(response);
+                break;
+            case '3':
+                toHost.println(messageMethod(subMessage, BookScrabblePort));
+                break;
+            default:
+                break;
+        }
+    }
+
+    private String messageMethod (String message, int port) { // A method that communicates with MyServer instances
         String response = null;
         try {
-            socket = new Socket(serverName, port);
-            games.put(this, socket);
+            Socket socket = new Socket("localhost", port);
 
             try (Scanner inFromServer = new Scanner(socket.getInputStream());
                  PrintWriter outToServer = new PrintWriter(socket.getOutputStream())) {
@@ -49,6 +129,12 @@ public class ServerManager {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        return response;
+        return response; // The response from the server
+    }
+
+    public void closeServerManager() {
+        closeServerManager = true;
+        gameServers.values().forEach(MyServer::close);
+        BookScrabbleServer.close();
     }
 }
